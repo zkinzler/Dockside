b3eea1e2c8b957781df8c5c42fd6c3a5758f8e38
Haley Holt <holthaley7@gmail.com>
2026-04-08 13:07:15 -0700

    Fix Vercel API handler

--- packages/client/api/index.ts
+++ packages/client/api/index.ts
@@ -1,3 +1,11 @@
 import app from "../../server/src/index";
+import { VercelRequest, VercelResponse } from "@vercel/node";
 
-export default app;
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  return new Promise((resolve, reject) => {
+    app(req as any, res as any, (err: any) => {
+      if (err) reject(err);
+      else resolve(undefined);
+    });
+  });
+}
